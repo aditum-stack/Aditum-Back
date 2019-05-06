2b27d5f816f47fcc866be7f1a0fa5d5421658404
kevinten10 <596823919@qq.com>
2019-05-06 20:26:59 +0800

    update

--- src/main/java/com/ten/aditum/back/AditumWebApplication.java
+++ src/main/java/com/ten/aditum/back/AditumWebApplication.java
@@ -1,11 +1,9 @@
 package com.ten.aditum.back;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableApolloConfig
 @SpringBootApplication
 @MapperScan("com.ten.aditum.back.mapper")
 public class AditumWebApplication {
--- src/main/java/com/ten/aditum/back/controller/ConfigController.java
+++ src/main/java/com/ten/aditum/back/controller/ConfigController.java
@@ -1,9 +1,6 @@
 package com.ten.aditum.back.controller;
 
-import com.ten.aditum.back.config.ApolloConfigBean;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
