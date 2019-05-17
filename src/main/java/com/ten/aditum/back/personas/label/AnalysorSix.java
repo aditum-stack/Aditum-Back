4a0322dd0139c91b52c6bed30fcdfa70849799d5
Kevin_T <596823919@qq.com>
2019-05-17 22:28:10 +0800

    update : finish @interval analyser

--- src/main/java/com/ten/aditum/back/personas/label/AnalysorSix.java
+++ src/main/java/com/ten/aditum/back/personas/label/AnalysorSix.java
@@ -1,21 +1,39 @@
 package com.ten.aditum.back.personas.label;
 
+import com.ten.aditum.back.entity.AccessInterval;
+import com.ten.aditum.back.entity.AccessInterval;
+import com.ten.aditum.back.entity.Person;
 import com.ten.aditum.back.entity.PersonasLabel;
 import com.ten.aditum.back.BaseAnalysor;
+import com.ten.aditum.back.service.AccessIntervalService;
+import com.ten.aditum.back.util.TimeGenerator;
+import com.ten.aditum.back.vo.Personas;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 基于排名的数据分析
+ * 基于访问时长的分析
  */
 @Slf4j
 @Component
 @EnableScheduling
 @EnableAutoConfiguration
 public class AnalysorSix extends BaseAnalysor {
+
+    private final AccessIntervalService accessIntervalService;
+
+    @Autowired
+    public AnalysorSix(AccessIntervalService accessIntervalService) {
+        this.accessIntervalService = accessIntervalService;
+    }
 
     @Override
     public void showModelLabel() {
@@ -40,8 +58,73 @@ public class AnalysorSix extends BaseAnalysor {
      */
     @Scheduled(cron = "0 50 4 1/1 * ?")
     public void analysis() {
-        log.info("开始更新社区信息...");
+        log.info("基于访问时长的分析...开始");
 
-        log.info("社区信息更新完成...");
+        List<Person> personList = selectAllPerson();
+
+        personList.forEach(this::analysisPerson);
+
+        log.info("基于访问时长的分析...开始");
+    }
+
+    private static final long HOUR = 60 * 60;
+
+    private void analysisPerson(Person person) {
+        // 获取AccessInterval
+        AccessInterval accessIntervalEntity = new AccessInterval()
+                .setPersonnelId(person.getPersonnelId())
+                .setIsDeleted(NO_DELETED);
+        List<AccessInterval> select = accessIntervalService.select(accessIntervalEntity);
+        if (select.size() < 1) {
+            log.info("此用户还没有AccessInterval记录, {}", person);
+            return;
+        }
+
+        // 获取AccessInterval
+        AccessInterval theAccessInterval = select.get(0);
+        // 平均在家滞留时间
+        String meanTimeRetention = theAccessInterval.getMeanTimeRetention();
+        // 平均外出工作时间
+        String meanTimeOut = theAccessInterval.getMeanTimeOut();
+
+        long in = TimeGenerator.getTotalSec(meanTimeRetention);
+        long out = TimeGenerator.getTotalSec(meanTimeOut);
+
+        if (in == 0 && out == 0) {
+            log.warn("此用户 {} 访问间隔为空，退出", person.getPersonnelName());
+        }
+
+        // 标签集合
+        List<String> labelSet = new ArrayList<>();
+        // 标签删除集合
+        List<String> removeSet = new ArrayList<>();
+
+        // 每天工作时间超过十小时
+        if (out > HOUR * 10) {
+            labelSet.add("高购买力人群");
+            Personas personas = new Personas()
+                    .setPersonnelId(person.getPersonnelId())
+                    .setLabelId("16");
+            personasController.updatePersonas(personas);
+        }
+        // 每天从晚上到早上在家10个小时
+        if (in > HOUR * 10) {
+            labelSet.add("倾向于待在家里");
+            Personas personas = new Personas()
+                    .setPersonnelId(person.getPersonnelId())
+                    .setLabelId("21");
+            personasController.updatePersonas(personas);
+        }
+        // 每天在外面12个小时
+        if (out > HOUR * 12) {
+            labelSet.add("在外时间长");
+            Personas personas = new Personas()
+                    .setPersonnelId(person.getPersonnelId())
+                    .setLabelId("22");
+            personasController.updatePersonas(personas);
+        }
+
+        log.info("用户 {} 计算完成。添加 : {} , 删除 : {}",
+                person.getPersonnelName(), String.join(",", labelSet), String.join(",", removeSet));
     }
 }
