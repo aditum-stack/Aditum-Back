e2854c4d38f0664aa0c39d1c6095dcacdec55461
Kevin_T <596823919@qq.com>
2019-05-20 22:09:11 +0800

    TODO @personasService

--- src/main/java/com/ten/aditum/back/service/PersonasService.java
+++ src/main/java/com/ten/aditum/back/service/PersonasService.java
@@ -2,19 +2,17 @@ package com.ten.aditum.back.service;
 
 import com.ten.aditum.back.entity.PersonasLabel;
 import com.ten.aditum.back.entity.PersonasPortrait;
-import com.ten.aditum.back.model.AditumCode;
-import com.ten.aditum.back.model.ResultModel;
 import com.ten.aditum.back.util.TimeGenerator;
 import com.ten.aditum.back.vo.Personas;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+
 
 @Slf4j
 @Service
@@ -37,27 +35,19 @@ public class PersonasService {
     /**
      * 根据personId更新该用户的用户画像标签
      */
-    public ResultModel updatePersonas(Personas personas) {
-        log.debug("Personas [POST] : {}", personas);
-
+    public void updatePersonas(Personas personas) {
         if (personas.getPersonnelId() == null || personas.getLabelId() == null) {
-            return new ResultModel(AditumCode.ERROR);
+            return;
         }
-
-        // select portrait
-        PersonasPortrait personasPortrait = new PersonasPortrait()
-                .setPersonnelId(personas.getPersonnelId())
-                .setIsDeleted(NO_DELETED);
-
-        List<PersonasPortrait> personasPortraitList = personasPortraitService.select(personasPortrait);
 
         // personas model array
         List<String> labelList = new ArrayList<>();
         boolean existed;
         int portraitId = 0;
 
+        PersonasPortrait personasPortrait = selectPortrait(personas);
         // 当前用户画像不存在
-        if (personasPortraitList.size() < 1) {
+        if (personasPortrait == null) {
             existed = false;
             labelList.add("新用户");
             labelList.add("青铜会员");
@@ -65,9 +55,8 @@ public class PersonasService {
         // 当前用户画像已存在
         else {
             existed = true;
-            PersonasPortrait select = personasPortraitList.get(0);
-            portraitId = select.getId();
-            String personasExt = select.getPersonasExt();
+            portraitId = personasPortrait.getId();
+            String personasExt = personasPortrait.getPersonasExt();
             String[] personasList = personasExt.split(",");
             labelList.addAll(Arrays.asList(personasList));
         }
@@ -80,7 +69,7 @@ public class PersonasService {
         List<PersonasLabel> personasLabelList = personasLabelService.select(personasLabel);
         if (personasLabelList.size() < 1) {
             log.warn("Personas [POST] Label FAILURE : {}", personas);
-            return new ResultModel(AditumCode.ERROR);
+            return;
         }
 
         PersonasLabel selectLabel = personasLabelList.get(0);
@@ -98,9 +87,6 @@ public class PersonasService {
                     .setPersonasExt(String.join(",", labelList))
                     .setUpdateTime(TimeGenerator.currentTime());
             personasPortraitService.update(update);
-
-            log.debug("Personas [POST] update SUCCESS : {} -> {}", personas, update);
-            return new ResultModel(AditumCode.OK, update);
         }
         // 未存在，创建
         else {
@@ -110,20 +96,15 @@ public class PersonasService {
                     .setCreateTime(TimeGenerator.currentTime())
                     .setIsDeleted(NO_DELETED);
             personasPortraitService.insert(create);
-
-            log.debug("Personas [POST] create SUCCESS : {} -> {}", personas, create);
-            return new ResultModel(AditumCode.OK, create);
         }
     }
 
     /**
      * 根据personId更新该用户的用户画像标签，可直接通过Name添加，而无需在Label表中有对应项
      */
-    public ResultModel updatePersonasByLabelName(Personas personas) {
-        log.debug("Personas by name [POST] : {}", personas);
-
+    public void updatePersonasByLabelName(Personas personas) {
         if (personas.getPersonnelId() == null || personas.getLabelName() == null) {
-            return new ResultModel(AditumCode.ERROR);
+            return;
         }
 
         // select portrait
@@ -168,9 +149,6 @@ public class PersonasService {
                     .setPersonasExt(String.join(",", labelList))
                     .setUpdateTime(TimeGenerator.currentTime());
             personasPortraitService.update(update);
-
-            log.debug("Personas by name [POST] update SUCCESS : {} -> {}", personas, update);
-            return new ResultModel(AditumCode.OK, update);
         }
         // 未存在，创建
         else {
@@ -180,52 +158,31 @@ public class PersonasService {
                     .setCreateTime(TimeGenerator.currentTime())
                     .setIsDeleted(NO_DELETED);
             personasPortraitService.insert(create);
-
-            log.debug("Personas by name [POST] create SUCCESS : {} -> {}", personas, create);
-            return new ResultModel(AditumCode.OK, create);
         }
     }
 
     /**
      * 根据personId删除该用户的用户画像标签
      */
-    public ResultModel removePersonas(Personas personas) {
-        log.debug("Personas [DELETE] : {}", personas);
-
+    public void removePersonas(Personas personas) {
         if (personas.getPersonnelId() == null || personas.getLabelId() == null) {
-            return new ResultModel(AditumCode.ERROR);
+            return;
+        }
+        PersonasPortrait personasPortrait = selectPortrait(personas);
+        if (personasPortrait == null) {
+            return;
+        }
+        PersonasLabel personasLabel = selectLabel(personas);
+        if (personasLabel == null) {
+            return;
         }
 
-        // select portrait
-        PersonasPortrait personasPortrait = new PersonasPortrait()
-                .setPersonnelId(personas.getPersonnelId())
-                .setIsDeleted(NO_DELETED);
-
-        List<PersonasPortrait> personasPortraitList = personasPortraitService.select(personasPortrait);
-        if (personasPortraitList.size() < 1) {
-            log.warn("Personas [DELETE] 当前用户画像不存在 : {}", personas);
-            return new ResultModel(AditumCode.ERROR);
-        }
-
-        PersonasPortrait select = personasPortraitList.get(0);
-        int portraitId = select.getId();
-        String personasExt = select.getPersonasExt();
+        int portraitId = personasPortrait.getId();
+        String personasExt = personasPortrait.getPersonasExt();
         String[] personasList = personasExt.split(",");
         List<String> labelList = new ArrayList<>(Arrays.asList(personasList));
 
-        // select model
-        PersonasLabel personasLabel = new PersonasLabel()
-                .setLabelId(personas.getLabelId())
-                .setIsDeleted(NO_DELETED);
-
-        List<PersonasLabel> personasLabelList = personasLabelService.select(personasLabel);
-        if (personasLabelList.size() < 1) {
-            log.warn("Personas [DELETE] Label FAILURE : {}", personas);
-            return new ResultModel(AditumCode.ERROR);
-        }
-
-        PersonasLabel selectLabel = personasLabelList.get(0);
-        String labelName = selectLabel.getLabelName();
+        String labelName = personasLabel.getLabelName();
 
         // 若包含此标签，删除
         labelList.remove(labelName);
@@ -235,9 +192,75 @@ public class PersonasService {
                 .setPersonasExt(String.join(",", labelList))
                 .setUpdateTime(TimeGenerator.currentTime());
         personasPortraitService.update(update);
+    }
 
-        log.debug("Personas [DELETE] remove SUCCESS : {} -> {}", personas, update);
-        return new ResultModel(AditumCode.OK, update);
+
+    /**
+     * 根据personId删除该用户的 模糊匹配 用户画像标签
+     */
+    public void removePersonasByKey(Personas personas) {
+        if (personas.getPersonnelId() == null || personas.getLabelName() == null) {
+            return;
+        }
+
+        PersonasPortrait personasPortrait = selectPortrait(personas);
+        if (personasPortrait == null) {
+            return;
+        }
+
+        String key = personas.getLabelName();
+
+        int portraitId = personasPortrait.getId();
+        String personasExt = personasPortrait.getPersonasExt();
+        String[] personasList = personasExt.split(",");
+        List<String> labelList = new ArrayList<>(Arrays.asList(personasList));
+
+        // 若包含此关键字，删除
+        Iterator iterator = labelList.iterator();
+        while (iterator.hasNext()) {
+            String label = (String) iterator.next();
+            if (label.contains(key)) {
+                iterator.remove();
+            }
+        }
+
+        PersonasPortrait update = new PersonasPortrait()
+                .setId(portraitId)
+                .setPersonasExt(String.join(",", labelList))
+                .setUpdateTime(TimeGenerator.currentTime());
+        personasPortraitService.update(update);
+    }
+
+    // -------------------------------------------------------------- private
+
+    /**
+     * 根据personnelId查询PersonasPortrait
+     */
+    private PersonasPortrait selectPortrait(Personas personas) {
+        PersonasPortrait personasPortrait = new PersonasPortrait()
+                .setPersonnelId(personas.getPersonnelId())
+                .setIsDeleted(NO_DELETED);
+        List<PersonasPortrait> personasPortraitList = personasPortraitService.select(personasPortrait);
+        if (personasPortraitList.size() < 1) {
+            log.warn("当前用户画像不存在 : {}", personas.getLabelName());
+            return null;
+        }
+        return personasPortraitList.get(0);
+    }
+
+    /**
+     * 根据personnelId查询PersonasLabel
+     */
+    private PersonasLabel selectLabel(Personas personas) {
+        PersonasLabel personasLabel = new PersonasLabel()
+                .setLabelId(personas.getLabelId())
+                .setIsDeleted(NO_DELETED);
+        List<PersonasLabel> personasLabelList = personasLabelService.select(personasLabel);
+        if (personasLabelList.size() < 1) {
+            log.warn("Personas [POST] Label FAILURE : {}", personas);
+            return null;
+        }
+        return personasLabelList.get(0);
     }
 
 }
