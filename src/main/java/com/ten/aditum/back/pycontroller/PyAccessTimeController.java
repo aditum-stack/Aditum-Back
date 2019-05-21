5799e36553b13e102bf404d098cd13936872f071
Kevin_T <596823919@qq.com>
2019-05-21 22:28:25 +0800

    update : update python runtime executor

--- src/main/java/com/ten/aditum/back/pycontroller/PyAccessTimeController.java
+++ src/main/java/com/ten/aditum/back/pycontroller/PyAccessTimeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @RestController
@@ -23,23 +24,37 @@ public class PyAccessTimeController {
     @RequestMapping(value = "/time", method = RequestMethod.GET)
     public ResultModel getTimeClustering() {
         log.info("PythonAccessTime [GET] clustering");
-        String line = null;
-        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
+
+        String base64 = null;
+        String[] arguments = new String[]{
+                PythonConstants.PYTHON_PATH,
+                "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
         try {
             Process process = Runtime.getRuntime().exec(arguments);
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-            }
+            // python只返回一行base64，故只取第一行
+            base64 = in.readLine();
+            base64 = new String(base64.getBytes(), StandardCharsets.UTF_8);
+            log.info("AccessTimeClusteringModel.py return base64 : {}", base64);
             in.close();
             int re = process.waitFor();
-            System.out.println(re);
+            if (re != 0) {
+                log.warn("AccessTimeClusteringModel.py throws exception");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        log.debug("PythonAccessTime [GET] clustering SUCCESS {}", line);
-        return new ResultModel(AditumCode.OK, line);
+        if (base64 == null) {
+            log.warn("PythonAccessTime base64 image is null");
+            return new ResultModel(AditumCode.ERROR);
+        }
+
+        String[] s = base64.split("'");
+        String base64Img = PythonConstants.BASE64_PREFIX + s[1];
+
+        log.info("PythonAccessTime [GET] clustering SUCCESS {}", base64Img);
+        return new ResultModel(AditumCode.OK, base64Img);
     }
 
 }
--- src/main/java/com/ten/aditum/back/pycontroller/PythonLocalTest.java
+++ src/main/java/com/ten/aditum/back/pycontroller/PythonLocalTest.java
@@ -2,16 +2,20 @@ package com.ten.aditum.back.pycontroller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class PythonLocalTest {
     public static void main(String[] args) {
         String line = null;
-        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
+        String[] arguments = new String[]{
+                "D:\\Users\\shihaowang\\AppData\\Local\\Programs\\Python\\Python37\\python.exe",
+                "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
         try {
             Process process = Runtime.getRuntime().exec(arguments);
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
             while ((line = in.readLine()) != null) {
-                System.out.println(line);
+                String str = new String(line.getBytes(), StandardCharsets.UTF_8);
+                System.out.println(str);
             }
             in.close();
             int re = process.waitFor();
