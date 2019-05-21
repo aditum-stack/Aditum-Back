8673d459809fcecb8e22aad459c984dea049462a
Kevin_T <596823919@qq.com>
2019-05-21 21:47:12 +0800

    update : update python runtime executor

--- src/main/java/com/ten/aditum/back/pycontroller/PyAccessTimeController.java
+++ src/main/java/com/ten/aditum/back/pycontroller/PyAccessTimeController.java
@@ -24,7 +24,7 @@ public class PyAccessTimeController {
     public ResultModel getTimeClustering() {
         log.info("PythonAccessTime [GET] clustering");
         String line = null;
-        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\strategy\\access\\AccessTimeClusteringModel.py"};
+        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
         try {
             Process process = Runtime.getRuntime().exec(arguments);
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
--- src/main/java/com/ten/aditum/back/pycontroller/PythonLocalTest.java
+++ src/main/java/com/ten/aditum/back/pycontroller/PythonLocalTest.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class PythonLocalTest {
     public static void main(String[] args) {
         String line = null;
-        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com\\ten\\aditum\\personas\\strategy\\access\\AccessTimeClusteringModel.py"};
+        String[] arguments = new String[]{"python", "D:\\Users\\shihaowang\\Desktop\\Aditum-Personas\\com.ten.aditum\\personas\\AccessTimeClusteringModel.py"};
         try {
             Process process = Runtime.getRuntime().exec(arguments);
             BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
